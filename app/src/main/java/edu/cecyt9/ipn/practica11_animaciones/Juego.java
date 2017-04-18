edcb13b497e94ca99a92bb3006495246be270a53
CECyT9 <CECyT9>
2017-04-18 07:55:00 -0500

    Initial commit

---
+++ app/src/main/java/edu/cecyt9/ipn/practica11_animaciones/Juego.java
@@ -0,0 +1,29 @@
+package edu.cecyt9.ipn.practica11_animaciones;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+
+public class Juego extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView texto = (TextView) findViewById(R.id.textView);
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.animacion);
+        texto.startAnimation(animacion);
+
+        TextView texto2 = (TextView) findViewById(R.id.textView2);
+        Animation cosa = AnimationUtils.loadAnimation(this, R.anim.cosa);
+        texto2.startAnimation(cosa);
+    }
+
+
+
+
+}
